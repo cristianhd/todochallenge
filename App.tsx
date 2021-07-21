b09d529f4b0a27cd472cdc0457fd1325f7698031
cristianhd <cristian.hd1996@gmail.com>
2021-07-21 15:13:26 -0300

    navigation

--- App.tsx
+++ App.tsx
@@ -2,14 +2,26 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import NavBar from "./src/components/NavBar/NavBar";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Form from './src/components/Form/Form';
+import Home from './src/components/Home/Home';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View>
-      <NavBar/>
+    
+    
+      <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Form" component={Form} />
+          </Stack.Navigator>
+      </NavigationContainer>
       
-      {/* <StatusBar style="auto" /> */}
-    </View>
+ 
+   
   );
 }
 
