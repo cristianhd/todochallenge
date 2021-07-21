60f9904c419e806e51a37f63569645cb04beb646
cristianhd <cristian.hd1996@gmail.com>
2021-07-21 16:44:59 -0300

    start form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,12 +1,124 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { Fragment } from "react";
+import { View, TextInput,Text } from "react-native";
+import { useForm, Controller } from "react-hook-form";
 
-
+type AddTask ={
+    title: string,
+    deadline: string,
+    startime: string,
+    endtime: string,
+    remind: string,
+    repeat: string
+}
 const Form =()=>{
+
+    const {control,handleSubmit} = useForm<AddTask>();
+
     return (
-        <View>
-            <Text>Form</Text>
-        </View>
+        <Fragment>
+            <Text>Title</Text>
+            <Controller
+                control={control}
+                rules={{
+                required: true,
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                    />
+                )}
+                name="title"
+                defaultValue=""
+            />
+
+            <Text>Deadline</Text>
+            <Controller
+                control={control}
+                rules={{
+                required: true,
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                    />
+                )}
+                name="deadline"
+                defaultValue=""
+            />
+            <View>
+                <Text>Start time</Text>
+                <Controller
+                    control={control}
+                    rules={{
+                    required: true,
+                    }}
+                    render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                            onBlur={onBlur}
+                            onChangeText={onChange}
+                            value={value}
+                        />
+                    )}
+                    name="startime"
+                    defaultValue=""
+                />
+
+                <Text>End time</Text>
+                <Controller
+                    control={control}
+                    rules={{
+                    required: true,
+                    }}
+                    render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                            onBlur={onBlur}
+                            onChangeText={onChange}
+                            value={value}
+                        />
+                    )}
+                    name="endtime"
+                    defaultValue=""
+                />
+            </View>
+
+            <Text>Remind</Text>
+            <Controller
+                control={control}
+                rules={{
+                required: true,
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                    />
+                )}
+                name="remind"
+                defaultValue=""
+            />
+
+            <Text>Repeat</Text>
+            <Controller
+                control={control}
+                rules={{
+                required: true,
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                    />
+                )}
+                name="repeat"
+                defaultValue=""
+            />
+        </Fragment>
     )
 };
 
